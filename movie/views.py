dd0c77be3cca950fbc90acad2f4189ddd9725d45
marendonq <miguelangelrq2005@gmail.com>
2024-07-31 23:52:31 -0500

    Visualizacion de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>si funciona</h1>')
-    return render(request, 'home.html',{'name':'Miguel Angel Rendon Quintero'})
-
+    #return render(request, 'home.html')
+    #return render(request, 'home.html',{'name':'Miguel Angel Rendon Quintero'})
+    searchTerm= request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies= Movie.objects.all()
+    return render(request, 'home.html',{'searchTerm':searchTerm, 'movies': movies})
 def about(request):
     #return HttpResponse('<h1>Welcome to about page</h1>')
     return render(request, 'about.html')
